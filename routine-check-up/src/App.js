68deca06da42f026667e8bb4a3cdf018937680a4
mahi-g <gm1585@nyu.edu>
2020-12-01 17:12:13 -0500

    Add routes using react router

--- routine-check-up/src/App.js
+++ routine-check-up/src/App.js
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import {
+  BrowserRouter as Router
+} from 'react-router-dom'
+import Routes from './routes/Routes'
 
 import { Container, Col, Row } from "shards-react";
 import Footer from './Components/Layout/Footer'
 import Navigation from './Components/Navigation'
-import TaskControl from './Components/Tasks/TaskControl'
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "shards-ui/dist/css/shards.min.css";
@@ -13,11 +16,13 @@ import "shards-ui/dist/css/shards.min.css";
 function App() {
   return (
     <div className="App">
-      <Navigation />
-      <Container style={{height: '100vh'}} fluid className="dr-example-container bg-secondary">
-        <TaskControl />
-      </Container>
-      <Footer />
+      <Router>
+        <Navigation />
+        <Container style={{height:'100vh'}} fluid className="dr-example-container bg-secondary">
+            <Routes />
+        </Container>
+        <Footer />
+      </Router>
     </div>
   );
 };
---
+++ routine-check-up/src/routes/Routes.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import {Link, Route, Switch} from 'react-router-dom'
+import TaskControl from '../Components/Tasks/TaskControl'
+
+
+const Routes = () => {
+    return(
+            <Switch>
+                <Route path="/routine"><TaskControl /></Route>
+                <Route path="/todays-tasks"><TaskControl /></Route>
+                <Route path="/manage-tasks"><TaskControl /></Route>
+                <Route path="/friends"><TaskControl /></Route>
+                <Route path="/" exact><TaskControl /></Route>
+                <Route>
+                    <div>
+                        <h1>404 Error</h1>
+                        <p>Go to <Link to="/">Home</Link></p>
+                    </div>
+                </Route>
+            </Switch>
+    )
+}
+
+export default Routes;
